f3390322825c704512a67ceddb50dd8352ef4bce
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-04 11:49:08 +0530

    Initial spring boot file setup

---
+++ src/test/java/com/EduPulse_AI/EduPulseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduPulse_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPulseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
